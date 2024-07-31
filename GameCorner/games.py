002187f49dd26ac03bd4e7191b6303185eaf57bd
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-07-31 12:44:24 +0800

    修改了 API 串接和一些函數

--- GameCorner/games.py
+++ GameCorner/games.py
@@ -3,16 +3,16 @@ import json
 import os
 import requests
 from load_env import Env
+import utils
 
-
+# 從api獲取遊戲列表
 def get_game_list(skip: int = 0, limit: int = 4) -> list:
-    # 從api獲取遊戲列表
     result = requests.post(Env.DB_SQL_API, headers={
             'content-type': 'application/json'
         },
         data=json.dumps({
             "sql_statement": f"""
-            SELECT id, name, description, cover_image FROM games order by id desc LIMIT {limit} OFFSET {skip};
+            SELECT id, name, description, cover_image FROM Games order by id desc LIMIT {limit} OFFSET {skip};
             """, # 以 id 降冪排序，配合懶加載，可達成較新的遊戲在前
             'query_mode': True,
             'return_as_dict': True
@@ -34,12 +34,20 @@ def get_game_list(skip: int = 0, limit: int = 4) -> list:
 
 # TODO: 上傳遊戲到資料庫
 # 本函數內容不正確
-def post_game_data(title, description, cover_image, game_file) -> None:
+def post_game_data(title, description, cover_image, entry_file,  game_files) -> None:
+    entry_file = utils.encode_base64(entry_file)
+
+    # TODO: cover_image 需先處理為 bytes 的形式
+    cover_image = utils.encode_base64(cover_image)
+
+    # TODO: game_files 需先處理為 {file_name(str): file_content(base64)} 的形式
+
     game_data: dict = {
-        "title": title,
+        "name": title,
         "description": description,
         "cover_image": cover_image,
-        "game_file": game_file
+        "entry_file": entry_file,
+        "game_file": game_files
     }
     # 將遊戲數據發送到api
     requests.post(Env.DB_RECORD_API, headers={
--- GameCorner/utils.py
+++ GameCorner/utils.py
@@ -3,16 +3,16 @@ import base64
 import re
 
 # 將圖片檔編碼為 Base64
-def encode_image_to_base64(image_path: str) -> str:
+def encode_local_image_to_base64(image_path: str) -> str:
     with open(image_path, "rb") as image_file:
         return encode_base64(image_file.read())
 
 # 編碼為 Base64
-def encode_base64(content: str) -> base64:
+def encode_base64(content: bytes | str) -> base64:
     return base64.b64encode(content.encode('utf-8')).decode('utf-8')
 
 # 解碼 Base64
-def decode_base64(content: base64) -> str:
+def decode_base64(content: base64) -> str | bytes:
     return base64.b64decode(content).decode('utf-8')
 
 # 清理路徑
