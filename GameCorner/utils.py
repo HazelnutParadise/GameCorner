45ef5a6b02ae8043a6f509c2b1771280fa30aa73
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-07-31 13:07:39 +0800

    排版更動

--- GameCorner/utils.py
+++ GameCorner/utils.py
@@ -2,23 +2,28 @@
 import base64
 import re
 
+
 # 將圖片檔編碼為 Base64
 def encode_local_image_to_base64(image_path: str) -> str:
     with open(image_path, "rb") as image_file:
         return encode_base64(image_file.read())
 
+
 # 編碼為 Base64
 def encode_base64(content: bytes | str) -> base64:
     return base64.b64encode(content.encode('utf-8')).decode('utf-8')
+
 
 # 解碼 Base64
 def decode_base64(content: base64) -> str | bytes:
     return base64.b64decode(content).decode('utf-8')
 
+
 # 清理路徑
 def clean_path(path):
     # 去除所有前缀，包括 .、./ 和 /
     return re.sub(r'^[./]+', '', path)
+
 
 # 將 HTML 內容中的資源標籤替換為嵌入(js/css)或後端連結(img)
 def process_html_tags(html_content, resources, backend_url):
@@ -77,6 +82,7 @@ def replace_css_imports(content, resources):
             content = content.replace(f'@import "{match}";', imported_content)
             content = replace_css_imports(content, resources)  # 遞迴替換
     return content
+
 
 # 將 JavaScript 內容中的 import/require 替換為嵌入內容或後端連結
 def replace_js_imports(content, resources, backend_url):
